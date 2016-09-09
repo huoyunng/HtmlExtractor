84e32ec51eb731e99c3b76af3b9f1e6a598ce792
wei.xu <wei.xu@thebeastshop.com>
2016-09-09 13:52:58 +0800

    test

---
+++ html-extractor/src/test/java/org/apdplat/extractor/BasicTest.java
@@ -0,0 +1,8 @@
+package org.apdplat.extractor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicTest<T> {
+	protected static final Logger LOGGER = LoggerFactory.getLogger(BasicTest.class);
+}
---
+++ html-extractor/src/test/java/org/apdplat/extractor/TestExtractor.java
@@ -0,0 +1,12 @@
+package org.apdplat.extractor;
+
+import org.junit.Test;
+
+public class TestExtractor extends BasicTest<TestExtractor>{
+	
+	@Test
+	public void testUsage3(){
+		LOGGER.info("asffaf");
+	}
+	
+}
